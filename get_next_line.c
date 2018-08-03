feacc9f8cfb7bb5e3ddf1f76d76c8286729402ae
Elizabeth Dehmlow <edehmlow@student.42.us.org>
2018-08-03 01:18:28 -0700

    rough draft of get next line

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,43 @@
 /*   By: edehmlow <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/24 21:58:54 by edehmlow          #+#    #+#             */
-/*   Updated: 2018/07/25 01:49:10 by edehmlow         ###   ########.fr       */
+/*   Updated: 2018/08/03 00:29:12 by edehmlow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int get_next_line(const int fd, char **line)
-{
-	int	rd;
+#include "get_next_line.h"
 
-	rd = read(fd, , 0);
+int	get_next_line(const int fd, char **line)
+{
+	static char		*a[65535];
+	char			*ptr;
+	char			*buff;
+
+	buff = ft_strnew((size_t)BUFF_SIZE);
+	//ft_memalloc a for max fd size?
+	if (read(fd, buff, 0) == -1)
+		return (-1);
+	if (!a[fd])
+		read(fd, a[fd], BUFF_SIZE);
+	if (a[fd])
+	{
+		while (!(ptr = ft_strchr(a[fd], '\n')) && read(fd, buff, BUFF_SIZE))
+		{
+			read(fd, buff, BUFF_SIZE);
+			a[fd] == ft_strjoin(a[fd], buff);
+		}
+		if (ptr = ft_strchr(a[fd], '\n'))
+		{
+			*line = ft_strndup(a[fd], ptr - buff);
+			ptr++;
+			a[fd] = ft_strdup(ptr);
+			return (1);
+		}
+		if (read(fd, buff, BUFF_SIZE) == 0)
+		{
+			free(a[fd]);
+			return (0);
+		}
+	}
 	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,18 +6,15 @@
 /*   By: edehmlow <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/24 21:59:13 by edehmlow          #+#    #+#             */
-/*   Updated: 2018/07/25 01:51:22 by edehmlow         ###   ########.fr       */
+/*   Updated: 2018/08/02 22:33:23 by edehmlow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE
+# define BUFF_SIZE 10
 # include <unistd.h>
 
 int get_next_line(const int fd, char **line);
-
-typedef struct	s_lst
-{
-}				t_lst;
 
 #endif
